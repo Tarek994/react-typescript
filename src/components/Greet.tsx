f8d89cc66e42ae8d9806d1139e3026ca5c2e6965
Tarek994 <ghali.tarek.123@hotmail.com>
2023-06-14 17:20:28 +0300

    feat(Greet.tsx): add the number and boolean props type

--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,9 +1,16 @@
 type GreetProps = {
     name: string
+    messageCount: number
+    isLoggedIn: boolean
 }
 
 export const Greet = (props : GreetProps) => {
   return (
-    <div>Greet {props.name}</div>
+    <div>
+      {props.isLoggedIn
+       ? ` ${props.name} and the num is ${props.messageCount}` 
+       : "no one here"}
+
+    </div>
   )
 }

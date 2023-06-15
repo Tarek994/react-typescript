f757548345d3cf5f2771d606509c26ff8d67abd4
Tarek994 <ghali.tarek.123@hotmail.com>
2023-06-16 00:30:27 +0300

    feat(app.tsx): add the data props for the person-list component

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,35 @@ import React from 'react';
 import './App.css';
 import { Greet } from './components/Greet.tsx';
 import { Person } from './components/Person.tsx';
+import { PersonList } from './components/PersonList.tsx';
 
 function App() {
   const personName = {
-    first: "barak",
-    last: " obama"
+    first: "Bruce",
+    last: " Wayne"
   }
+
+  const nameList = [
+    {
+      first: "Bruce",
+      last: " Wayne"
+    },
+    {
+      first: "Clark",
+      last: " Kent"
+    },
+    {
+      first: "Princess",
+      last: "Diana"
+    }
+  ]
+
 
   return (
     <div className="App">
-      <Greet name="welcome Tarek" messageCount={10} isLoggedIn={false}/>
-      <Person name={personName}/>
+      <Greet name="welcome Tarek" messageCount={10} isLoggedIn={false} />
+      <Person name={personName} />
+      <PersonList names={nameList} />
     </div>
   );
 }

43a3f9e52e7fa16247baa3b42e0b02aa60c93cad
Tarek994 <ghali.tarek.123@hotmail.com>
2023-06-15 23:58:12 +0300

    feat(app.tsx): import thr person component and add the props to it

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
 import { Greet } from './components/Greet.tsx';
+import { Person } from './components/Person.tsx';
 
 function App() {
+  const personName = {
+    first: "barak",
+    last: " obama"
+  }
+
   return (
     <div className="App">
       <Greet name="welcome Tarek" messageCount={10} isLoggedIn={false}/>
+      <Person name={personName}/>
     </div>
   );
 }

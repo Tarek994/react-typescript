fabc2d7911c1a7833a6ab5fae4f4bd2ace4f72a7
Tarek994 <ghali.tarek.123@hotmail.com>
2023-06-16 23:58:30 +0300

    feat(app.tsx): add the handle-change props to input component's props

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import { Greet } from './components/Greet.tsx';
 import { Person } from './components/Person.tsx';
 import { PersonList } from './components/PersonList.tsx';
+import Button from './components/Button.tsx';
+import Input from './components/Input.tsx';
 
 function App() {
   // const personName = {
@@ -35,6 +37,7 @@ function App() {
       <Button handleClick={(event, id) => {
         console.log('button Clicked', event);
       }} />
+      <Input value='' handleChange={(event) => console.log(event)}/>
     </div>
   );
 }

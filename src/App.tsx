ca75cbbe94715619946b9b0ad95b19a57afc60d8
Tarek994 <ghali.tarek.123@hotmail.com>
2023-06-16 23:09:55 +0300

    chore(app.tsx): comment out unused code

--- src/App.tsx
+++ src/App.tsx
@@ -5,32 +5,32 @@ import { Person } from './components/Person.tsx';
 import { PersonList } from './components/PersonList.tsx';
 
 function App() {
-  const personName = {
-    first: "Bruce",
-    last: " Wayne"
-  }
+  // const personName = {
+  //   first: "Bruce",
+  //   last: " Wayne"
+  // }
 
-  const nameList = [
-    {
-      first: "Bruce",
-      last: " Wayne"
-    },
-    {
-      first: "Clark",
-      last: " Kent"
-    },
-    {
-      first: "Princess",
-      last: "Diana"
-    }
-  ]
+  // const nameList = [
+  //   {
+  //     first: "Bruce",
+  //     last: " Wayne"
+  //   },
+  //   {
+  //     first: "Clark",
+  //     last: " Kent"
+  //   },
+  //   {
+  //     first: "Princess",
+  //     last: "Diana"
+  //   }
+  // ]
 
 
   return (
     <div className="App">
-      <Greet name="welcome Tarek" messageCount={10} isLoggedIn={false} />
+      {/* <Greet name="welcome Tarek" messageCount={10} isLoggedIn={false} />
       <Person name={personName} />
-      <PersonList names={nameList} />
+      <PersonList names={nameList} /> */}
     </div>
   );
 }

c91e1bc8ee38b07c9432145ae0a9618c93e07073
Tarek994 <ghali.tarek.123@hotmail.com>
2023-06-18 18:29:35 +0300

    feat(container.tsx): add the style using props

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,7 +1,12 @@
+type ContainerProps = {
+    styles: React.CSSProperties
+     
+}
 
-export const Container = () => {
+
+export const Container = (props: ContainerProps) => {
   return (
-    <div style={{border: '1px solid black', padding: '1rem'}}>
+    <div style={props.styles}>
      Text content goes here
      </div>
   )

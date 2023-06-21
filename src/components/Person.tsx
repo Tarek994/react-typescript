fdc4d78d9e405d4fc7b224b60195b12203393ff7
Tarek994 <ghali.tarek.123@hotmail.com>
2023-06-21 03:01:36 +0300

    feat(person.tsx): change the type props with import from another component

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,10 +1,4 @@
-
-type PersonProps = {
-  name : {
-    first: string
-    last: string
-  }
-}
+import { PersonProps } from "./Person.type.ts"
 
 export const Person = (props: PersonProps) => {
   return (

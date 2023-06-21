c156fb930e15c3d80b06f2b480610374995b6813
Tarek994 <ghali.tarek.123@hotmail.com>
2023-06-21 17:29:46 +0300

    feat(LoggedIn.tsx): add login and logout functionality

--- src/components/state/LoggedIn.tsx
+++ src/components/state/LoggedIn.tsx
@@ -3,3 +3,9 @@ import { useState } from "react"
 export const LoggedIn = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
 
+  const handelLogin = () => {
+    setIsLoggedIn(true)
+  }
+  const handelLogout = () => {
+    setIsLoggedIn(false)
+  }

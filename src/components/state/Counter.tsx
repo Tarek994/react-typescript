eff5d5a0af5ccd199445f4b353c2e639424a4469
Tarek994 <ghali.tarek.123@hotmail.com>
2023-06-25 23:40:23 +0300

    feat(Counter.tsx): add reducer function

--- src/components/state/Counter.tsx
+++ src/components/state/Counter.tsx
@@ -2,6 +2,14 @@ import {useReducer} from 'react'
 
 const initialState = { count: 0}
 
+function reducer(state, action) {
+    switch (action.type){
+        case'increment':
+            return {count: state.count + action.payload}
+        case'decrement':
+            return {count: state.count + action.payload}
+        }
+}
 
 
 export const Counter = () => {

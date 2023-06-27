a084ccf07b767622d077bf928ed4d0b7b8a69a70
Tarek994 <ghali.tarek.123@hotmail.com>
2023-06-27 23:52:17 +0300

    feat(Counter.tsx): add the return of the useReducer function

--- src/components/state/Counter.tsx
+++ src/components/state/Counter.tsx
@@ -1,6 +1,6 @@
 import {useReducer} from 'react'
 
-const initialState = { count: 0}
+const initialState = { count: 0 }
 
 function reducer(state, action) {
     switch (action.type){
@@ -8,6 +8,8 @@ function reducer(state, action) {
             return {count: state.count + action.payload}
         case'decrement':
             return {count: state.count + action.payload}
+            default:
+              return state
         }
 }
 
@@ -16,7 +18,13 @@ export const Counter = () => {
   const [state, dispatch] = useReducer(reducer, initialState)
   return (
     <>
-
+      Count: {state.count}
+      <button onClick={() => dispatch({type: 'increment', payload: 10 })}>
+        Increment 10
+      </button>
+      <button onClick={() => dispatch({type: 'decrement', payload: 10 })}>
+        Increment 10
+      </button>
     </>
   )
 }

647d730e4cb9603a590c77567d34ed0d1d3b971e
Tarek994 <ghali.tarek.123@hotmail.com>
2023-07-03 23:59:10 +0300

    feat(Box.tsx): add the theme with the useContext hook

--- src/components/context/Box.tsx
+++ src/components/context/Box.tsx
@@ -1,6 +1,9 @@
+import { useContext } from 'react'
+import { ThemeContext } from "./ThemeContext"
 import React from 'react'
 
 export const Box = () => {
+    const theme = useContext(ThemeContext)
   return (
     <div>Box</div>
   )

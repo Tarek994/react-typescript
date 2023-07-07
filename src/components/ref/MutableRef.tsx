0d8365239fd458a5bd43b54d3eaa2747ae4626de
Tarek994 <ghali.tarek.123@hotmail.com>
2023-07-07 23:19:12 +0300

    feat(MutableRef.tsx): add timer state and interval ref

---
+++ src/components/ref/MutableRef.tsx
@@ -0,0 +1,13 @@
+import {useState, useRef, useEffect} from 'react'
+
+export const MutableRef = () => {
+    const [timer, setTimer] = useState(0)
+    const interValRef = useRef(null)
+
+    
+
+
+  return (
+    <div>MutableRef</div>
+  )
+}

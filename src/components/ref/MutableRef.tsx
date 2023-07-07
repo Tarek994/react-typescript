1b929559f6cae239c1d17dd5dcc422465293c7f5
Tarek994 <ghali.tarek.123@hotmail.com>
2023-07-07 23:34:10 +0300

    feat(MutableRef.tsx): implement timer functionality using useEffect

--- src/components/ref/MutableRef.tsx
+++ src/components/ref/MutableRef.tsx
@@ -1,13 +1,20 @@
-import {useState, useRef, useEffect} from 'react'
+import { useState, useRef, useEffect } from 'react'
 
 export const MutableRef = () => {
     const [timer, setTimer] = useState(0)
     const interValRef = useRef(null)
 
-    
 
+    useEffect(() => {
+        interValRef.current = window.setInterval(() => {
+            setTimer((timer) => timer + 1)
+        }, 1000)
+        return () => {
+            stopTimer()
+        }
+    }, [])
 
-  return (
-    <div>MutableRef</div>
-  )
+    return (
+        <div>MutableRef</div>
+    )
 }

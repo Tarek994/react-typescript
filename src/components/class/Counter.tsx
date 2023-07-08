fbc11b4442742ff5ae0d7d32f1d02170a85c7389
Tarek994 <ghali.tarek.123@hotmail.com>
2023-07-08 23:51:03 +0300

    feat(Counter.tsx): add the Counter Class Component

---
+++ src/components/class/Counter.tsx
@@ -0,0 +1,18 @@
+import { Component } from 'react'
+
+
+
+export class Counter extends Component {
+    state = {
+        count: 0,
+    }
+
+
+    render() {
+            <div>
+                
+            </div>
+        )
+    }
+
+}

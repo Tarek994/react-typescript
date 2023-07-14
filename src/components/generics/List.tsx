06e7959ea33463b7e87f85631eab1c15143e0311
Tarek994 <ghali.tarek.123@hotmail.com>
2023-07-14 23:58:47 +0300

    feat(list.tsx): change the props type of the list component

--- src/components/generics/List.tsx
+++ src/components/generics/List.tsx
@@ -1,9 +1,9 @@
-type ListProps = {
-    items: string[] | number[]
-    onClick: (value: string | number) => void
+type ListProps<T> = {
+    items: T[]
+    onClick: (value:T) => void
 }
 
-export const List = ({ items, onClick }: ListProps) => {
+export const List = <T extends {}>({ items, onClick }: ListProps<T>) => {
     return (
         <div>
             <h2>List of items</h2>

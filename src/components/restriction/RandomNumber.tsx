4076ef25b1d10238afc0135b54748e75dcd673e7
Tarek994 <ghali.tarek.123@hotmail.com>
2023-07-22 22:28:35 +0300

    refector(RandomNumber): use discriminated unions

--- src/components/restriction/RandomNumber.tsx
+++ src/components/restriction/RandomNumber.tsx
@@ -20,12 +20,8 @@ type Zero = RandomNumberType & {
     isNegative?: never
 }
 
-type RandomNumberProps = {
-    value: number
-    isPositive?: boolean
-    isNegative?: boolean
-    isZero?: boolean
-}
+type RandomNumberProps = PositiveNumber | NegativeNumber | Zero
+
 
 export const RandomNumber = ({
     value,
